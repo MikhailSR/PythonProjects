88d46ee49e174bc1d41d6ad2766578bc4bbcb694
Mikhail <manufriev927@gmail.com>
2022-01-21 02:00:20 +0200

    modifided file main.py

--- Differend projects/Tkinter calculator/main.py
+++ Differend projects/Tkinter calculator/main.py
@@ -3,6 +3,9 @@ import tkinter as tk
 window = tk.Tk()
  
 entry = tk.Entry(width=40, bg="white", fg="black")
+button = tk.Button()
+
+button.pack()
 entry.pack()
  
 entry.insert(0, "What is your name?")
